5fd81902e39fc90b085f53870544abc19a574448
Noam <Noam.Leabovich@gmail.com>
2024-11-24 17:19:03 +0200

    -Adding insert_at

--- insert_none_at.ml
+++ insert_none_at.ml
@@ -1,1 +1,6 @@
+let rec insert_at = fun e i l -> match i, l with
+        |0,_ -> e::l
+        |_, h::t -> h::insert_at e (i-1) t
+        |_, [] -> l;;
+
 let insert_none_at = fun i l -> insert_at "None" i l;;
